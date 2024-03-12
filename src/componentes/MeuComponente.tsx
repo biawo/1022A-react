fcb60b179d411737126d33157847630cd59b9eca
biawo <bianca.moraes@estudante.ifms.edu.br>
2024-03-12 10:12:34 -0400

    Meu Primeiro Commit

---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,14 @@
+//Faça um componente que faça um retângulo da largura da tela por 200px
+//Crie um variável com let no javascript que contenha o valor "Meu Componente"
+//Crie uma div no componente que dentro dela mostre um <p> contendo o valor da variável
+
+export default MeuComponente;
+import "./MeuComponente.css"
+function MeuComponente() {
+    let variavel = "Meu componente"
+    return (
+      <> 
+<div className = "retangulo"><p>{variavel}</p></div>
+      </>
+    )
+  }
