5a0a87c4deee11109b72c4eceb942a2764f08539
biawo <bianca.moraes@estudante.ifms.edu.br>
2024-03-19 10:14:38 -0400

    Box estudante na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,10 +1,19 @@
 export default Main;
+import "./Main.css"
 
 function Main(){
     return(
         <>
-        <main>Main</main>
-        <h1>meu h1</h1>
+        <main>
+            <div className="estudante-box">
+                <img className="estudante-foto" src = "https://github.com/biawo.png" alt=""/>
+                <div className="informacoes">
+                <h3 className="estudante_nome">Bianca de Oliveira Moraes</h3>
+                <p className="estudante_descricao">Estudante do IFMS</p>
+                <a className="link-git" href="https://github.com/">GitHub.</a>
+                </div>
+            </div>
+        </main>
         </>
     )
 }
